430ec98f71fae0f9e6dd8dc736bcdcadd9e7730a
Antônio Rafael <antonio.rafael0415@gmail.com>
2026-08-08 12:24:26 -0300

    feat: render projects dinamically

---
+++ src/javascript/render-projects.js
@@ -0,0 +1,46 @@
+const projectsSection = document.getElementById("projects");
+
+const fetchProjects = async () => {
+  try {
+    const response = await fetch("../src/data/projects.json");
+
+    if (!response.ok) return;
+
+    const data = await response.json();
+    renderProjects(data);
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const renderProjects = (projects) => {
+  projectsSection.innerHTML = ``;
+
+  projects.forEach((project) => {
+    const projectContainer = document.createElement("div");
+    projectContainer.classList.add("project")
+    const tags = project.tags;
+
+    projectContainer.innerHTML = `
+            <img src="" alt="Imagem do projeto ${project.name}">
+            <h2>${project.name}</h2>
+            <div class="tags-container">
+                ${tags
+                  .map(
+                    (tag) => `<div class="tag">${tag}</div>
+                    `,
+                  )
+                  .join("")}
+            </div>
+            <p>${project.description}</p>
+            <div class="project-links">
+                <a href="${project.siteUrl}" target="_blank">Site</a>
+                <a href="${project.repositoryUrl}" target="_blank">Repositório</a>
+            </div>
+        `;
+    projectsSection.appendChild(projectContainer);
+  });
+};
+
+fetchProjects();
